dc7358e3f0bffb8b2db736eec50c08a506e7f024
Luan Duarte <luansouza789@gmail.com>
2024-01-23 18:26:15 -0300

    finish availaility and blocked-dates endpoints

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../../lib/prisma'
-import dayjs from 'dayjs'
 
 export default async function handle(
   req: NextApiRequest,
@@ -42,13 +41,26 @@ export default async function handle(
     )
   })
 
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT * 
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT
+      EXTRACT(DAY FROM S.date) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
     FROM schedulings S
 
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+
     WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, '%Y-%m') = ${year}-${month}
+      AND DATE_FORMAT(S.date, '%Y-%m') = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.date),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
   `
 
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+  const blockedDates = blockedDatesRaw.map((blockedDate) => blockedDate.date)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
